f5c7532128cf1ef4e0462f44cad70e6c9ba255b2
cam <cjmooredev@gmail.com>
2025-01-01 15:01:51 -0400

    basic gameloop, gitignore files

--- main.py
+++ main.py
@@ -2,8 +2,23 @@ import pygame
 from constants import *
 
 def main():
+    # initialize pygame
     pygame.init()
     print(f"Starting asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+
+    # make the screen
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        # if quit event occurs exit the game loop
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        #fill screen with black color represented as r, g, b, a set
+        screen.fill([0, 0, 0, 0])
+        # refresh screen
+        pygame.display.flip()
+
 
 if __name__ == "__main__":
     main()
