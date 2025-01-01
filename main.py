42f94b5358791b65a82398a018a59c18cadac106
cam <cjmooredev@gmail.com>
2025-01-01 14:34:22 -0400

    corrected constants and initialized pygame

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 from constants import *
 
 def main():
+    pygame.init()
     print(f"Starting asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
